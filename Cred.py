db0a0a77285b08ed51f07361d632d385a72496af
Prakharjain007 <jainprakhar0712@gmail.com>
2023-07-20 03:02:55 +0530

    first

---
+++ Cred.py
@@ -0,0 +1,4 @@
+def Cred():
+    mysql_pass="5555"
+    db="email"
+    return mysql_pass,db
---
+++ Tables.py
@@ -0,0 +1,45 @@
+from Cred import Cred
+import mysql.connector as ms
+
+def mysql_connection():
+    password,db =  Cred() 
+    mycon=ms.connect(host="localhost",user="root",db=db,passwd=password)
+    cur=mycon.cursor()
+    return mycon,cur
+
+def logintable(mycon,cur):
+    sql='''
+    create table login(
+    first_name varchar(50) Not Null,
+    last_name varchar(50),
+    username varchar(50) primary key,
+    password varchar(50)
+    )
+    '''
+    cur.execute(sql)
+    mycon.commit()
+
+def email(mycon,cur):
+    sql ='''
+    create table mails(
+    s_no int primary key auto_increment,
+    mail_date varchar(10) not null,
+    sender varchar(50) not null,
+    receiver varchar(50) not null,
+    subject varchar(50),
+    text varchar(500)
+    )
+    '''
+    cur.execute(sql)
+    mycon.commit()
+
+
+def main():
+    mycon,cur = mysql_connection()
+    logintable(mycon,cur)
+    email(mycon,cur)
+    print("Table Created Successfully")
+    
+
+if(__name__ == "__main__"):
+    main()
