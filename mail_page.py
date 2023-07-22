38d6107f3c0b1b2a25d1438264f6ce94c1f8e4e7
Vibhuj01 <jainvibhu.2002@gmail.com>
2023-07-22 22:14:07 +0530

    Alola

--- mail_page.py
+++ mail_page.py
@@ -3,42 +3,15 @@ import datetime
 from tabulate import tabulate
 
 def mail_page(uname):
-
-<<<<<<< HEAD
-    while(True):
-        print("1. Compose")
-        print("2. Check Sent ")
-        print("3. Inbox")
-        print("4. Change Password")
-        print("5. Logout") 
-
-        choice= input("What do you want to perform: ")
-        print("\n--------------------------------------------\n")
-
-        if(choice=="1"):
-            compose(uname)
-        elif(choice=='2'):
-            sent(uname)
-        elif(choice=="3"):
-            inbox(uname)
-        elif(choice=="4"):
-            cgpswd()
-        elif(choice == "5"):
-            return 
-        else:
-            print("Wrong Input!!! ")
-            print("Try Again")
-=======
     print("1. Compose")
     print("2. Check Sent ")
     print("3. Inbox")
     print("4. Change Password")
-    print("5. Logout")
+    print("5. Logout") 
 
     choice= input("What do you want to perform: ")
     print("\n--------------------------------------------\n")
->>>>>>> 8dc2ee1324b9b45424e9282acf8d016f2ebfd5b2
-
+    
     if(choice=="1"):
         compose(uname)
     elif(choice=='2'):
@@ -58,28 +31,38 @@ def mail_page(uname):
 def compose(uname):
     mycon,cur = mysql_connection()
     reciver_mail= input("Enter Reciever's Mail ID: ")
-<<<<<<< HEAD
-=======
     reciver_mail = reciver_mail.lower()
->>>>>>> 8dc2ee1324b9b45424e9282acf8d016f2ebfd5b2
     sub= input("Enter Subject of the Mail: ")
     text=input("Enter the mail text: ")
     print("\n--------------------------------------------\n")
     date=datetime.date.today()
-<<<<<<< HEAD
-    sql='Insert into mails (mail_date,sender,receiver,subject,text) values(%s,%s,%s,%s,%s)'
-    data=[date,uname,reciver_mail,sub,text]
-    try:
+    
+    sql= 'select * from login'
+    cur.execute(sql)
+    result= cur.fetchall() 
+    exists = False
+    for row in result:
+        if(row[2] == reciver_mail):
+            exists = True
+            break
 
-        cur.execute(sql,data)
-        mycon.commit()
-
-    except:
-        print("Something is Wrong!!!!")
-        print("\n--------------------------------------------\n")
     else:
-        print("Mail Sent Successfully")
+        print("Username doesn't Exists, Enter a valid username")
         print("\n--------------------------------------------\n")
+
+    if(exists == True):
+        try:
+            sql='Insert into mails (mail_date,sender,receiver,subject,text) values(%s,%s,%s,%s,%s)'
+            data=[date,uname,reciver_mail,sub,text]
+            cur.execute(sql,data)
+            mycon.commit()
+
+        except:
+            print("Something is Wrong!!!!")
+            print("\n--------------------------------------------\n")
+        else:
+            print("Mail Sent Successfully")
+            print("\n--------------------------------------------\n")
 
 def sent(uname):
     mycon,cur=mysql_connection()
@@ -121,33 +104,3 @@ def inbox(uname):
         keys=["Serial No.","Date","Sender","Reciever","Subject","Mail"]
         print(tabulate(mails, headers = keys, tablefmt = 'pretty', showindex = False))
         print("\n--------------------------------------------\n")
-
-=======
-    
-    sql= 'select * from login'
-    cur.execute(sql)
-    result= cur.fetchall() 
-    exists = False
-    for row in result:
-        if(row[2] == reciver_mail):
-            exists = True
-            break
-
-    else:
-        print("Username doesn't Exists, Enter a valid username")
-        print("\n--------------------------------------------\n")
-
-    if(exists == True):
-        try:
-            sql='Insert into mails (mail_date,sender,receiver,subject,text) values(%s,%s,%s,%s,%s)'
-            data=[date,uname,reciver_mail,sub,text]
-            cur.execute(sql,data)
-            mycon.commit()
-
-        except:
-            print("Something is Wrong!!!!")
-            print("\n--------------------------------------------\n")
-        else:
-            print("Mail Sent Successfully")
-            print("\n--------------------------------------------\n")
->>>>>>> 8dc2ee1324b9b45424e9282acf8d016f2ebfd5b2
