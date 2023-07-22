5849d22b3ce271d269414f57a12d0590baaffe17
Vibhuj01 <jainvibhu.2002@gmail.com>
2023-07-22 21:20:31 +0530

    Added Validation

--- mail_page.py
+++ mail_page.py
@@ -31,9 +31,9 @@ def mail_page(uname):
 
 def compose(uname):
     mycon,cur = mysql_connection()
-    reciver_mail= input("Enter Reciever's Mail ID")
-    sub= input("Enter Subject of the Mail")
-    text=input("Enter the mail text")
+    reciver_mail= input("Enter Reciever's Mail ID: ")
+    sub= input("Enter Subject of the MailL: ")
+    text=input("Enter the Mail Text: ")
     sql='Insert into mails values(%s,%s,%s)'
     data=[uname,reciver_mail,sub,text]
     cur.execute(sql,data)
